3d405f7949e024b8064a47dca8ac336aca0fc0a5
Benjamin Phang <BenPhang@Benjamins-MacBook-Pro.local>
2016-09-05 13:10:21 +0800

    tested with correct response for first challenge

--- deaf_aunty.rb
+++ deaf_aunty.rb
@@ -8,7 +8,26 @@
 # You'll probably want to write other methods, but this
 # encapsulates the core Aunty logic
 def deaf_aunty
+
+	puts "Hello there sweetie, is there anything you wanted to say?"
+
+	loop do
+		ans = gets.chomp
+		comp = ans.upcase
+
+		if ans == comp 
+		puts "NO, WE CAN'T DO THAT!"
+		elsif ans == "I love ya, aunty, but I've got to go." 
+		puts "bye"
+		break
+		else 
+		puts "HUH?! SPEAK UP,SANDRA!"
+		end
+	end
 end
+
 
 # Run our method
 deaf_aunty
+
+
